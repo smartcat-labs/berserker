669f2ff0d1b0cd4a15a594922d60bbc5dcc4b147
Vladimir Vajda <vladimir.vajda@smartcat.io>
2018-04-19 09:57:57 +0200

    Added support for both sync and async HTTP worker; Added additional configuration properties (closes #48)

--- berserker-http/README.md
+++ berserker-http/README.md
@@ -2,9 +2,21 @@
 
 Worker implementation which sends HTTP request on configured endpoint.
 
-Configuration can define two properties:
-1. `base-url` - can be concatenated with request property `url-sufix` to construct url.
-2. `headers` - contains header names with its values.
+Configuration can define following properties:
+1. `async` - Can be `true` or `false`. Determines whether messages will be sent in asynchronous fashion or not. Optional, if not specified defaults to `false`.
+2. `keep-alive` - Can be `true` or `false`. Determines whether HTTP keep-alive is enabled or disabled. Optional, defaults to `true`.
+3. `max-connections` - The maximum number of connections a HTTP client can handle, or `-1` for no connection limit. Optional, if not specified defaults to `-1`.
+4. `max-connections-per-host` - The maximum number of connections per host a HTTP client can handle, or `-1` for no connection limit. Optional, if not specified defaults to `-1`.
+5. `connect-timeout` - The maximum time in millisecond a HTTP client can wait when connecting to a remote host. Optional, if not specified defaults to `5000`.
+6. `read-timeout` - The maximum time in millisecond a HTTP client can stay idle. Optional, if not specified defaults to `60000`.
+7. `pooled-connection-idle-timeout` - The maximum time in millisecond a HTTP client will keep connection in pool. Optional, if not specified defaults to `60000`.
+8. `request-timeout` - The maximum time in millisecond a HTTP client waits until the response is completed. Optional, if not specified defaults to `60000`.
+9. `follow-redirect` - Can be `true` or `false`. Determines whether HTTP redirect is enabled. Optional, if not specified defaults to `true`.
+10. `max-redirects` - The maximum number of HTTP redirects. Optional, if not specified defaults to `5`.
+11. `max-request-retry` - The number of time the library will retry when an error occurs by the remote server. Optional, if not specified defaults to `5`.
+12. `connection-ttl` - The maximum time in millisecond a HTTP client will keep connection in the pool, or `-1` to keep connection while possible. Optional, if not specified defaults to `-1`.
+13. `base-url` - Can be concatenated with request property `url-sufix` to construct URL. Optional, depending on whether `url` or `url-sufix` is specified.
+14.. `headers` - Contains headers in a form of name-value map which will be added to each request. Optional.
 
 Worker `accept` method expects following properties:
 1. `url` - whole url to be used, it ignores `base-url`. Mutually exclusive with `url-sufix`.
@@ -19,6 +31,18 @@ Example yaml configuration:
 
 ```yaml
 worker-configuration:
+  async: false
+  keep-alive: true
+  max-connections: -1
+  max-connections-per-host: -1
+  connect-timeout: 5000
+  read-timeout: 60000
+  pooled-connection-idle-timeout: 60000
+  request-timeout: 60000
+  follow-redirect: true
+  max-redirects: 5
+  max-request-retry: 5
+  connection-ttl: -1
   base-url: http://localhost:8080/api/item
   headers:
     Content-Type: application/json
--- berserker-http/src/main/java/io/smartcat/berserker/http/worker/HttpWorker.java
+++ berserker-http/src/main/java/io/smartcat/berserker/http/worker/HttpWorker.java
@@ -27,6 +27,7 @@ public class HttpWorker implements Worker<Map<String, Object>> {
     private static final String HEAD = "HEAD";
     private static final List<String> METHOD_TYPES = Arrays.asList(GET, POST, PUT, DELETE, HEAD);
 
+    private final boolean async;
     private final String baseUrl;
     private final Map<String, String> headers;
 
@@ -35,14 +36,40 @@ public class HttpWorker implements Worker<Map<String, Object>> {
     /**
      * Constructs HTTP worker with specified properties.
      *
-     * @param baseUrl Can be concatenated with request property <code>url-sufix</code> to constructs url.
+     * @param async Indicates whether HTTP worker should behave in asynchronous or synchronous manner.
+     * @param keepAlive Indicates whether HTTP keep-alive is enabled or not.
+     * @param maxConnections The maximum number of connections an {@link AsyncHttpClient} can handle.
+     * @param maxConnectionsPerHost The maximum number of connections per host an AsyncHttpClient can handle.
+     * @param connectTimeout The maximum time in millisecond an {@link AsyncHttpClient} can wait when connecting to a
+     *            remote host.
+     * @param readTimeout The maximum time in millisecond an {@link AsyncHttpClient} can stay idle.
+     * @param pooledConnectionIdleTimeout The maximum time in millisecond an {@link AsyncHttpClient} will keep
+     *            connection in pool.
+     * @param requestTimeout The maximum time in millisecond an {@link AsyncHttpClient} waits until the response is
+     *            completed.
+     * @param followRedirect Indicates whether HTTP redirect is enabled.
+     * @param maxRedirects The maximum number of HTTP redirects.
+     * @param maxRequestRetry The number of time the library will retry when an {@link java.io.IOException} is thrown by
+     *            the remote server.
+     * @param connectionTtl The maximum time in millisecond an {@link AsyncHttpClient} will keep connection in the pool,
+     *            or -1 to keep connection while possible.
+     * @param baseUrl Can be concatenated with request property <code>url-sufix</code> to constructs URL.
      * @param headers Map of headers to use for each request.
      */
-    public HttpWorker(String baseUrl, Map<String, String> headers) {
+    public HttpWorker(boolean async, boolean keepAlive, int maxConnections, int maxConnectionsPerHost,
+            int connectTimeout, int readTimeout, int pooledConnectionIdleTimeout, int requestTimeout,
+            boolean followRedirect, int maxRedirects, int maxRequestRetry, int connectionTtl, String baseUrl,
+            Map<String, String> headers) {
+        this.async = async;
         this.baseUrl = baseUrl;
         this.headers = headers;
 
-        AsyncHttpClientConfig config = new DefaultAsyncHttpClientConfig.Builder().build();
+        AsyncHttpClientConfig config = new DefaultAsyncHttpClientConfig.Builder().setKeepAlive(keepAlive)
+                .setMaxConnections(maxConnections).setMaxConnectionsPerHost(maxConnectionsPerHost)
+                .setConnectTimeout(connectTimeout).setReadTimeout(readTimeout)
+                .setPooledConnectionIdleTimeout(pooledConnectionIdleTimeout).setRequestTimeout(requestTimeout)
+                .setFollowRedirect(followRedirect).setMaxRedirects(maxRedirects).setMaxRequestRetry(maxRequestRetry)
+                .setConnectionTtl(connectionTtl).build();
         asyncHttpClient = new DefaultAsyncHttpClient(config);
     }
 
@@ -73,7 +100,7 @@ public class HttpWorker implements Worker<Map<String, Object>> {
         String calculatedUrl = getCalculatedUrl(url, urlSufix);
         Map<String, String> calculatedHeaders = getCalculatedHeaders(requestHeaders);
 
-        asyncHttpClient.executeRequest(
+        ListenableFuture<Response> responseFuture = asyncHttpClient.executeRequest(
                 createRequest(methodType, calculatedUrl, calculatedHeaders, body),
                 new AsyncCompletionHandler<Response>() {
                     @Override
@@ -86,6 +113,13 @@ public class HttpWorker implements Worker<Map<String, Object>> {
                         throw new RuntimeException(t);
                     }
                 });
+        if (!async) {
+            try {
+                responseFuture.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
